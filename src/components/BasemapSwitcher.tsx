afa9cb185f8b3d97e2f069cdbb03e77c8d2e4e62
Riki Ruswandi <rikiruswandi28@gmail.com>
2025-09-26 14:53:48 +0000

    style:button color

--- src/components/BasemapSwitcher.tsx
+++ src/components/BasemapSwitcher.tsx
@@ -41,7 +41,7 @@ export default function BasemapSwitcher({ map, tileLayer }: BasemapSwitcherProps
   if (!tileLayer) return null;
 
   return (
-    <div className="basemap-switcher ol-control ol-unselectable bg-card/80 backdrop-filter-[4px] rounded-md p-0.5">
+    <div className="bg-card/80 backdrop-filter-[4px] rounded-md p-0.5">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="sm" className='w-[2.25rem] h-[2.25rem]'>
